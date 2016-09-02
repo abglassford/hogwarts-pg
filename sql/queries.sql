6d8659acf97b3f56ab8ac0376c45f9c4c1cf104d
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-01 19:43:27 -0600

    add some extra queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,11 +1,16 @@
 -- * How many students attend Hogwarts?
+
 SELECT count(*) FROM students;
+
 -- * How many students are in a specific house?
+
 SELECT name AS house_name, count(*) AS total_students FROM houses
 JOIN students
 ON house_id = houses.id
 GROUP BY name;
+
 -- * How many students are taking a class by a given teacher?
+
 SELECT t.first_name || ' ' || t.last_name AS teacher_name , count(student_id)
 FROM teachers t
 JOIN classes
@@ -16,43 +21,49 @@ JOIN students s
 ON student_id = s.id
 WHERE t.first_name = 'Serverus'
 GROUP BY t.first_name, t.last_name;
+
 -- * How many students are of a given year?
+
 SELECT * FROM students WHERE year = 2;
+
 -- * Which students have no parents?
 SELECT * FROM students s
 WHERE parent_id IS NULL;
 
--- extra queries
+-- extra queries -------------------------------------
 
-SELECT first_name, last_name
-FROM students
-WHERE (SELECT child_id FROM parents WHERE last_name = 'Diggory') = id;
+-- Who is Cedric Diggory's parent?
 
--- first_name | last_name
--- ------------+-----------
--- Cedric     | Diggory
-
-SELECT s.first_name, s.last_name
+SELECT p.first_name || ' ' || p.last_name AS parent
 FROM students s
 JOIN parents p
-ON s.id = child_id
+ON s.parent_id = p.id
 WHERE s.last_name = 'Diggory';
 
--- first_name | last_name
--- ------------+-----------
--- Cedric     | Diggory
+-- Which studends belong to Gryffindor?
 
-SELECT first_name, last_name, houses.name FROM teachers
-JOIN houses
-ON house_id = houses.id;
---
--- first_name | last_name  |    name
--- ------------+------------+------------
--- Serverus   | Snape      | Slytherin
--- Vincent    | Crabble    | Slytherin
--- Minerva    | McGonagall | Gryffindor
--- Remus      | Lupin      | Gryffindor
--- Pomona     | Sprout     | Hufflepuff
--- Silvanus   | Kettleburn | Hufflepuff
--- Filius     | Flitwick   | Ravenclaw
--- Roger      | Davies     | Ravenclaw
+SELECT h.name, s.first_name || ' ' || s.last_name AS student
+FROM students s
+JOIN houses h
+ON h.id = house_id
+WHERE h.name = 'Gryffindor';
+
+-- What is the Id of the teacher who teaches "The Light and How to Swing It?"
+
+SELECT t.id, t.first_name || ' ' || t.last_name AS teacher
+FROM teachers t
+JOIN classes c
+ON t.id = c.teacher_id
+WHERE c.subject = 'The Light and How to Swing It';
+
+-- Whose children are taking the class 'How to Not Die,' and what are the children's names?
+
+SELECT  p.first_name || ' ' || p.last_name AS parents, s.first_name || ' ' || s.last_name AS students
+FROM students s
+JOIN classes_students cs
+ON s.id = cs.student_id
+JOIN parents p
+ON p.id = s.parent_id
+JOIN classes c
+ON cs.class_id = c.id
+WHERE c.subject = 'How to Not Die';
